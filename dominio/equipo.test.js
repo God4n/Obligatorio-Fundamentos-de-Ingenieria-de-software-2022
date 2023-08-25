5b8d5d86e80ae4452329c5d6076ed3516a899743
Tadeo Goday <tadeo.goday@gmail.com>
2023-08-25 18:28:32 -0300

    Adjusting for GHPage

--- dominio/equipo.test.js
+++
@@ -1,7 +0,0 @@
-import Equipo from './equipo.mjs';
-
-test('creacion de un equipo', () => {
-  const unEquipo = new Equipo('Uruguay', 'http://url.prueba');
-  expect(unEquipo.pais).toBe('Uruguay');
-  expect(unEquipo.escudo).toBe('http://url.prueba');
-});
